f8a0dbd82712fddfee04e220038ae78f69aa9f25
Jean-François Lamy <jf@jflamy.dev>
2023-01-26 22:48:05 -0500

    init done.

--- src/main/java/app/owlcms/firmata/Main.java
+++ src/main/java/app/owlcms/firmata/Main.java
@@ -58,11 +58,11 @@ public class Main {
 			dsr.readPinDefinitions(workbook);
 			var emitPinDefinitions = dsr.getEmitPinDefinitions();
 			var buttonPinDefinitions = dsr.getButtonPinDefinitions();
-			
+
 			initBoard(board);
 			initModes(board, emitPinDefinitions, buttonPinDefinitions);
 
-			// showPinConfig(board);
+			showPinConfig(board);
 
 			startupLED(board);
 
@@ -94,7 +94,6 @@ public class Main {
 					System.err.println(message);
 				}
 			});
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -104,17 +103,17 @@ public class Main {
 			List<ButtonPinDefinition> buttonPinDefinition) {
 		emitPinDefinition.stream().forEach(i -> {
 			try {
-				logger.warn("emit {}",i.getPinNumber());
+				logger.warn("emit {}", i.getPinNumber());
 				Pin pin = board.getPin(i.getPinNumber());
 				pin.setMode(Mode.OUTPUT);
-				pin.setValue(0L);
+//				pin.setValue(0L);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		});
 		buttonPinDefinition.stream().forEach(i -> {
 			try {
-				logger.warn("button {}",i.getPinNumber());
+				logger.warn("button {}", i.getPinNumber());
 				board.getPin(i.getPinNumber()).setMode(Mode.PULLUP);
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -172,7 +171,7 @@ public class Main {
 	private static void showPinConfig(IODevice board) throws IOException {
 		for (int i = 0; i < board.getPinsCount(); i++) {
 			Pin pin = board.getPin(i);
-			System.err.println(i + " " + pin.getSupportedModes());
+			logger.warn("{} {}", i, pin.getSupportedModes());
 			if (pin.getMode() == Mode.ANALOG) {
 				if (pin.getSupportedModes().contains(Mode.OUTPUT)) {
 					pin.setMode(Mode.OUTPUT);
@@ -185,14 +184,15 @@ public class Main {
 		long now = System.currentTimeMillis();
 		long end = ignoredUntil[index - 1];
 		if (value == 1 && (now > end)) {
-			//logger.trace("ok now={} end={} : elapsed {}", now, end, now - end);
-			
+			// logger.trace("ok now={} end={} : elapsed {}", now, end, now - end);
+
 			// valid press.
 			// we start a new blocked duration to avoid stutter
 			ignoredUntil[index - 1] = now + DEBOUNCE_DURATION; // wait
 			return true;
 		} else {
-			//logger.trace("blocked {} now={} end={} : {} {}", value, now, end, (end - now) < 0 ? "elapsed" : "remaining", Math.abs(end - now));
+			// logger.trace("blocked {} now={} end={} : {} {}", value, now, end, (end - now)
+			// < 0 ? "elapsed" : "remaining", Math.abs(end - now));
 		}
 		return false;
 	}
