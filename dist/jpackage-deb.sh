2b3a3037287c83225565b5635f3329db77e270e4
Jean-François Lamy <jf@fjlamy.dev>
2023-02-06 18:58:05 -0500

    Linux script

---
+++ dist/jpackage-deb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -
+
+# x.y.z  x = major, y = minor
+# z = 0 : normal release
+# z > 0 : bug fix release
+# z00 .. z19 = alpha
+# z20 .. z39 = beta
+# z40 .. z49 = rc
+# z90 .. z99 = release
+
+VERSION=1.0.020
+
+#(cd ../../firmata4j; mvn -DskipTests install)
+(cd ..; mvn -Pproduction clean package)
+
+cp ../target/owlcms-firmata.jar files
+cp -r ../src/main/resources/devices files
+
+#jpackage --type exe --input files --main-jar owlcms-firmata.jar --main-class app.owlcms.firmata.ui.Main \
+# --name owlcms-firmata --icon files/owlcms.ico --runtime-image jre \
+# --win-menu --win-menu-group owlcms --win-console  --win-dir-chooser \
+# --app-version ${VERSION} --win-per-user-install --win-shortcut 
+
+jpackage --type pkg --input files --main-jar owlcms-firmata.jar --main-class app.owlcms.firmata.ui.Main \
+ --name owlcms-firmata --icon files/owlcms.png --runtime-image jre \
+ --app-version ${VERSION} 
