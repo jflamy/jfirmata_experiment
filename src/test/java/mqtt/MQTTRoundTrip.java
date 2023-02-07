75b55e4a36acc2589953529a45432082cfb33aab
Jean-François Lamy <jf@jflamy.dev>
2023-02-07 15:24:26 -0500

    moquette bug report

--- src/test/java/mqtt/MQTTRoundTrip.java
+++ src/test/java/mqtt/MQTTRoundTrip.java
@@ -12,13 +12,11 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
-import app.owlcms.firmata.ui.Main;
-import app.owlcms.firmata.utils.Config;
 import ch.qos.logback.classic.Logger;
 
 public class MQTTRoundTrip {
-	private static final String OWLCMS_FOP = "owlcms/fop/#";
-	Logger logger = (Logger) LoggerFactory.getLogger(MQTTRoundTrip.class);
+	private static final String OWLCMS_FOP = "owlcms/test/#";
+	static Logger logger = (Logger) LoggerFactory.getLogger(MQTTRoundTrip.class);
 
 	public class MQTTTestCallback implements MqttCallback {
 
@@ -29,8 +27,8 @@ public class MQTTRoundTrip {
 		@Override
 		public void messageArrived(String topic, MqttMessage message) throws Exception {
 			String messageStr = new String(message.getPayload(), StandardCharsets.UTF_8);
-            long before = Long.parseLong(messageStr);
-            logger.warn("round trip timing = {}ms", System.currentTimeMillis() - before);  
+			long before = Long.parseLong(messageStr);
+			logger.warn("round trip timing = {}ms", System.currentTimeMillis() - before);
 		}
 
 		@Override
@@ -40,54 +38,54 @@ public class MQTTRoundTrip {
 	}
 
 	private MQTTTestCallback callback = new MQTTTestCallback();
-	
+
 	public static MqttClient createMQTTClient(String fopName) throws MqttException {
-		String server = Config.getCurrent().getMqttServer();
-		server = (server != null ? server : "127.0.0.1");
-		String port = Config.getCurrent().getMqttPort();
-		port = (port != null ? port : "1883");
+		String server = "127.0.0.1";
+		String port =  "1883";
 		String protocol = port.startsWith("8") ? "ssl://" : "tcp://";
-		Main.getStartupLogger().info("connecting to MQTT {}{}:{}", protocol, server, port);
+		logger.info("connecting to MQTT {}{}:{}", protocol, server, port);
 
 		MqttClient client = new MqttClient(protocol + server + ":" + port,
 				fopName + "_" + MqttClient.generateClientId(), // ClientId
 				new MemoryPersistence()); // Persistence
 		return client;
 	}
-	
-    private MqttConnectOptions setUpConnectionOptions(String username, String password) {
-        MqttConnectOptions connOpts = new MqttConnectOptions();
-        connOpts.setCleanSession(true);
-        if (username != null) {
-            connOpts.setUserName(username);
-        }
-        if (password != null) {
-            connOpts.setPassword(password.toCharArray());
-        }
-        connOpts.setCleanSession(true);
-        // connOpts.setAutomaticReconnect(true);
-        return connOpts;
-    }
+
+	private MqttConnectOptions setUpConnectionOptions(String username, String password) {
+		MqttConnectOptions connOpts = new MqttConnectOptions();
+		connOpts.setCleanSession(true);
+		if (username != null) {
+			connOpts.setUserName(username);
+		}
+		if (password != null) {
+			connOpts.setPassword(password.toCharArray());
+		}
+		connOpts.setCleanSession(true);
+		// connOpts.setAutomaticReconnect(true);
+		return connOpts;
+	}
 
 	@Test
 	public void testRoundTrip() throws InterruptedException, MqttException {
 		Thread t1 = new Thread(() -> {
 			try {
 				MqttClient client = createMQTTClient("A");
-		        MqttConnectOptions connOpts = setUpConnectionOptions("test", "test");
-		        client.connect(connOpts);
+				MqttConnectOptions connOpts = setUpConnectionOptions("test", "test");
+				client.connect(connOpts);
 				client.setTimeToWait(-1);
 				client.setCallback(callback);
 				client.subscribe(OWLCMS_FOP, 0);
-				long before = System.currentTimeMillis();
-				client.publish("owlcms/fop/startup/A",
-						new MqttMessage(Long.toString(before).getBytes(StandardCharsets.UTF_8)));
+				for (int i = 0; i < 20; i++) {
+					long before = System.currentTimeMillis();
+					client.publish("owlcms/test/A",
+							new MqttMessage(Long.toString(before).getBytes(StandardCharsets.UTF_8)));
+				}
 				client.disconnect();
 			} catch (MqttException e) {
 			}
 		});
 		t1.start();
-		t1.join(5000);
+		t1.join(20000);
 	}
 
 }
