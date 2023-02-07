16df6cd3d88ffbd6f871b7989642bfc09a296760
Jean-François Lamy <jf@jflamy.dev>
2023-02-07 11:24:25 -0500

    round-trip time

---
+++ src/test/java/mqtt/MQTTRoundTrip.java
@@ -0,0 +1,93 @@
+package mqtt;
+
+import java.nio.charset.StandardCharsets;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import app.owlcms.firmata.ui.Main;
+import app.owlcms.firmata.utils.Config;
+import ch.qos.logback.classic.Logger;
+
+public class MQTTRoundTrip {
+	private static final String OWLCMS_FOP = "owlcms/fop/#";
+	Logger logger = (Logger) LoggerFactory.getLogger(MQTTRoundTrip.class);
+
+	public class MQTTTestCallback implements MqttCallback {
+
+		@Override
+		public void connectionLost(Throwable cause) {
+		}
+
+		@Override
+		public void messageArrived(String topic, MqttMessage message) throws Exception {
+			String messageStr = new String(message.getPayload(), StandardCharsets.UTF_8);
+            long before = Long.parseLong(messageStr);
+            logger.warn("round trip timing = {}ms", System.currentTimeMillis() - before);  
+		}
+
+		@Override
+		public void deliveryComplete(IMqttDeliveryToken token) {
+		}
+
+	}
+
+	private MQTTTestCallback callback = new MQTTTestCallback();
+	
+	public static MqttClient createMQTTClient(String fopName) throws MqttException {
+		String server = Config.getCurrent().getMqttServer();
+		server = (server != null ? server : "127.0.0.1");
+		String port = Config.getCurrent().getMqttPort();
+		port = (port != null ? port : "1883");
+		String protocol = port.startsWith("8") ? "ssl://" : "tcp://";
+		Main.getStartupLogger().info("connecting to MQTT {}{}:{}", protocol, server, port);
+
+		MqttClient client = new MqttClient(protocol + server + ":" + port,
+				fopName + "_" + MqttClient.generateClientId(), // ClientId
+				new MemoryPersistence()); // Persistence
+		return client;
+	}
+	
+    private MqttConnectOptions setUpConnectionOptions(String username, String password) {
+        MqttConnectOptions connOpts = new MqttConnectOptions();
+        connOpts.setCleanSession(true);
+        if (username != null) {
+            connOpts.setUserName(username);
+        }
+        if (password != null) {
+            connOpts.setPassword(password.toCharArray());
+        }
+        connOpts.setCleanSession(true);
+        // connOpts.setAutomaticReconnect(true);
+        return connOpts;
+    }
+
+	@Test
+	public void testRoundTrip() throws InterruptedException, MqttException {
+		Thread t1 = new Thread(() -> {
+			try {
+				MqttClient client = createMQTTClient("A");
+		        MqttConnectOptions connOpts = setUpConnectionOptions("test", "test");
+		        client.connect(connOpts);
+				client.setTimeToWait(-1);
+				client.setCallback(callback);
+				client.subscribe(OWLCMS_FOP, 0);
+				long before = System.currentTimeMillis();
+				client.publish("owlcms/fop/startup/A",
+						new MqttMessage(Long.toString(before).getBytes(StandardCharsets.UTF_8)));
+				client.disconnect();
+			} catch (MqttException e) {
+			}
+		});
+		t1.start();
+		t1.join(5000);
+	}
+
+}
