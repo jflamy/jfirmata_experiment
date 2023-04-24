656cfeea85a4fed33e69c551aff3fbd5c2faaec3
Jean-François Lamy <jf@jflamy.dev>
2023-04-24 12:38:34 -0400

    use busywait

--- src/main/java/app/owlcms/firmata/piezo/Tone.java
+++ src/main/java/app/owlcms/firmata/piezo/Tone.java
@@ -3,8 +3,6 @@ package app.owlcms.firmata.piezo;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 import org.firmata4j.Pin;
 import org.slf4j.LoggerFactory;
@@ -26,58 +24,58 @@ public class Tone {
 		logger.setLevel(Level.DEBUG);
 	}
 
-	public Thread play() {
-		double upDuration;
-		long nbFullCycles;
-		long squareNanos;
-
-		if (frequency == 0) {
-			// 250Hz silence
-			upDuration = 1.0D / 250;
-			nbFullCycles = (long) (((msDuration / 1000D) / upDuration));
-			squareNanos = Math.round((upDuration) * 1_000_000_000);
-		} else {
-			upDuration = (1.0D / frequency);
-			nbFullCycles = (long) (((msDuration / 1000D) / upDuration));
-			squareNanos = Math.round((upDuration) * 1_000_000_000);
-		}
-
-		var t1 = new Thread(() -> {
-			var lock = new ReentrantLock();
-			var done = lock.newCondition();
-			lock.lock();
-			var start = System.currentTimeMillis();
-			logger.warn("pin {} frequency {} upDuration {}s nanos {}ns cycles {}", pin.getIndex(), frequency, Double.toString(upDuration), squareNanos, nbFullCycles);
-
-			try {
-				for (int i = 0; i < nbFullCycles; i++) {
-					setPin(squareNanos, done, frequency > 0 ? 1 : 0);
-					setPin(squareNanos, done, 0);
-				}
-				//done.signal();
-			} catch (IllegalStateException | IOException | InterruptedException e) {
-				logger.error("exception {}", e);
-			} finally {
-				logger.warn("done {}", System.currentTimeMillis() - start);
-				lock.unlock();
-			}
-		});
-		t1.start();
-		return t1;
-	}
+//	public Thread play() {
+//		double upDuration;
+//		long nbFullCycles;
+//		long squareNanos;
+//
+//		if (frequency == 0) {
+//			// 250Hz silence
+//			upDuration = 1.0D / 250;
+//			nbFullCycles = (long) (((msDuration / 1000D) / upDuration));
+//			squareNanos = Math.round((upDuration) * 1_000_000_000);
+//		} else {
+//			upDuration = (1.0D / frequency);
+//			nbFullCycles = (long) (((msDuration / 1000D) / upDuration));
+//			squareNanos = Math.round((upDuration) * 1_000_000_000);
+//		}
+//
+//		var t1 = new Thread(() -> {
+//			var lock = new ReentrantLock();
+//			var done = lock.newCondition();
+//			lock.lock();
+//			var start = System.currentTimeMillis();
+//			logger.debug("pin {} frequency {} upDuration {}s nanos {}ns cycles {}", pin.getIndex(), frequency, Double.toString(upDuration), squareNanos, nbFullCycles);
+//
+//			try {
+//				for (int i = 0; i < nbFullCycles; i++) {
+//					setPin(squareNanos, done, frequency > 0 ? 1 : 0);
+//					setPin(squareNanos, done, 0);
+//				}
+//				//done.signal();
+//			} catch (IllegalStateException | IOException | InterruptedException e) {
+//				logger.error("exception {}", e);
+//			} finally {
+//				logger.debug("done {}", System.currentTimeMillis() - start);
+//				lock.unlock();
+//			}
+//		});
+//		t1.start();
+//		return t1;
+//	}
 	
 	public List<String> nanoTimes = new ArrayList<>();
 
-	private void setPin(long squareNanos, Condition done, int value) throws IOException, InterruptedException {
-		var now = System.nanoTime();
-		pin.setValue(value);
-		var wasted = System.nanoTime() - now;
-		int remaining = (int) (squareNanos - wasted);
-		if (remaining > 0) {
-			done.awaitNanos(squareNanos);
-		}
-		nanoTimes.add("late "+(System.nanoTime()-(now+squareNanos)));
-	}
+//	private void setPin(long squareNanos, Condition done, int value) throws IOException, InterruptedException {
+//		var now = System.nanoTime();
+//		pin.setValue(value);
+//		var wasted = System.nanoTime() - now;
+//		int remaining = (int) (squareNanos - wasted);
+//		if (remaining > 0) {
+//			done.awaitNanos(squareNanos);
+//		}
+//		nanoTimes.add("late "+(System.nanoTime()-(now+squareNanos)));
+//	}
 
 	public Thread playWait() {
 		double upDuration;
@@ -96,7 +94,7 @@ public class Tone {
 		}
 
 		var start = System.nanoTime();
-		logger.warn("pin {} frequency {} upDuration {}s nanos {}ns cycles {}", pin.getIndex(), frequency, Double.toString(upDuration), squareNanos, nbFullCycles);
+		logger.debug("pin {} frequency {} upDuration {}s nanos {}ns cycles {}", pin.getIndex(), frequency, Double.toString(upDuration), squareNanos, nbFullCycles);
 
 		var t1 = new Thread(() -> {
 			try {
