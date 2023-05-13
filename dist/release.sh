01f231b06e56bd744a1a00da7d3dc263f50b8699
Jean-François Lamy <jf@jflamy.dev>
2023-05-13 17:36:33 -0400

    commit jar

--- dist/release.sh
+++ dist/release.sh
@@ -44,3 +44,4 @@ gh release delete $TAG -y
 gh release create $TAG --notes-file ../RELEASE.md -t "owlcms-firmata $TAG"
 gh release upload $TAG *.exe
 find ../diagrams -name '*.xlsx' -print0 | xargs -0 -n 1 gh release upload --clobber $TAG
+gh release upload $TAG files/owlcms-firmata.jar
